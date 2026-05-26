0fc4fee94022f4b498e5045c6fd916af9040f359
joaopuel <67071957+joaopuel@users.noreply.github.com>
2026-05-25 21:56:50 -0300

    feat: Configuração incial do projeto

---
+++ src/test/java/com/ia/para/devs/bookingflow/BookingflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.para.devs.bookingflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
